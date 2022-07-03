5ef19e0fc2f367f112ae387279043538d08e5f35
Uliana Komarynska <uliakom@gmail.com>
2022-07-03 17:03:45 +0300

    task 4

---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+
+export const TransactionTable = styled.table`
+  width: 900px;
+}
+`;
+
+export const TableHead = styled.thead`
+  width: 100%;
+  tr {
+    width: 100%;
+  }
+
+  th {
+    padding: 15px;
+    border: 1px solid #ffffff;
+    text-align: center;
+    color: #ffffff;
+    font-family: Quicksand;
+    font-weight: 500;
+    font-size: 24px;
+    background-color: #4ddbd7;
+    box-shadow: 28px -2px 91px 26px rgba(23, 23, 23, 0.3) inset;
+    -webkit-box-shadow: 28px -2px 91px 26px rgba(23, 23, 23, 0.3) inset;
+    -moz-box-shadow: 28px -2px 91px 26px rgba(23, 23, 23, 0.3) inset;
+  }
+`;
+
+export const TableBody = styled.tbody`
+  width: 100%;
+  tr {
+    &: nth-child(2n) {
+      background: rgb(34, 193, 195);
+      background: linear-gradient(
+        0deg,
+        rgba(34, 193, 195, 0.2861519607843137) 15%,
+        rgba(255, 255, 255, 0.5270483193277311) 100%
+      );
+    }
+  }
+
+  td {
+    padding: 15px;
+    text-align: center;
+    font-family: Quicksand;
+    font-weight: 300;
+    font-size: 16px;
+  }
+`;
--- src/components/TransactionHistory/index.jsx
+++ src/components/TransactionHistory/index.jsx
@@ -1,7 +1,42 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import {
+  TransactionTable,
+  TableHead,
+  TableBody,
+} from './TransactionHistory.styled';
 
-const TransactionHistory = () => {
-  return <p></p>;
+const TransactionHistory = ({ items }) => {
+  return (
+    <TransactionTable>
+      <TableHead>
+        <tr>
+          <th>TYPE</th>
+          <th>AMOUNT</th>
+          <th>CURRENCY</th>
+        </tr>
+      </TableHead>
+      <TableBody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type.toUpperCase()}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </TableBody>
+    </TransactionTable>
+  );
 };
 
 export default TransactionHistory;
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
